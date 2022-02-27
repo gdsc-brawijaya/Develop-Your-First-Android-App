8da39aceab4695b44eb40cce2426fa0607e5b59b
ACER <loilyxtra@gmail.com>
2022-02-27 22:47:17 +0700

    Intent

--- app/src/main/java/com/gdsc/gdscubworkshopandroid1/MainActivity.kt
+++ app/src/main/java/com/gdsc/gdscubworkshopandroid1/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.gdsc.gdscubworkshopandroid1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImplicit: Button
+    private lateinit var btnExplicit: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImplicit = findViewById(R.id.btn_intent_implicit)
+        btnExplicit = findViewById(R.id.btn_intent_explicit)
+
+        btnImplicit.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"))
+            startActivity(intent)
+        }
+
+        btnExplicit.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
